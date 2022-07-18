245d8bfa94d1526dda45e1ccbfac836f1a43a82e
Wukualam22 <wukualam22@gmail.com>
2022-07-18 19:01:01 +0700

    adding file

---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-3.mp4' autoPlay loop muted />
+      <h1>VIEZTA ALAM</h1>
+      <p>Kami menyediakan jasa pembuatan kolam yang berkualitas tinggi dengan harga yang terjangkau.
+      </p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          HUBUNGI KAMI
+        </Button>
+        {/* <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button> */}
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
